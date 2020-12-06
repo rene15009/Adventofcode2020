f3807ac09e6be26bfcec8bad643192d3405191b5
René Pacios <rpacios@webrene.es>
2020-12-06 11:07:45 +0100

    Day 6 o adventofCode

--- 06.AdventOfCode/ModuloDay06.cs
+++ 06.AdventOfCode/ModuloDay06.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,7 +8,10 @@ namespace AdventOfCode
     internal static class ModuloDay06
     {
 
-
+        /// <summary>
+        /// return one group by item
+        /// </summary>
+        /// <returns></returns>
         private static IEnumerable<string> LoadData()
         {
             var appPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
@@ -19,11 +21,62 @@ namespace AdventOfCode
                     .ReadLines(filePath, Encoding.UTF8)
                 ;
 
-            return lineas;
+            string group = string.Empty;
+            foreach (var linea in lineas)
+            {
+                if (string.IsNullOrEmpty(linea.Trim()))
+                {
+                    yield return group;
+                    group = string.Empty;
+                }
+                else
+                {
+                    group = $"{group}{linea}";
+                }
+
+            }
+
+
+            if (!string.IsNullOrEmpty(group)) yield return group;
 
 
         }
 
+
+
+        /// <summary>
+        /// return one group by item
+        /// </summary>
+        /// <returns></returns>
+        private static IEnumerable<List<string>> LoadDataInGroups()
+        {
+            var appPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string filePath = Path.Combine(appPath!, "data.txt");
+
+            var lineas = File
+                    .ReadLines(filePath, Encoding.UTF8)
+                ;
+
+            List<string> group = new List<string>();
+            foreach (var linea in lineas)
+            {
+                if (string.IsNullOrEmpty(linea.Trim()))
+                {
+                    yield return group;
+                    group = new List<string>();
+                }
+                else
+                {
+                    group.Add(linea);
+                }
+
+            }
+
+
+            if (@group.Any()) yield return group;
+
+
+        }
 
 
 
@@ -32,7 +85,7 @@ namespace AdventOfCode
             var data = LoadData();
 
 
-            return data.Count();
+            return data.Sum(s => s.CountDifernetLetters());
 
         }
 
@@ -41,12 +94,40 @@ namespace AdventOfCode
         public static long SolveTwo()
         {
 
-            var data = LoadData();
-            return data.Count();
+            var data = LoadDataInGroups();
 
+            return data.Sum(s => s.CountSameLettersInGroup());
 
         }
 
+
+        private static int CountDifernetLetters(this string group)
+        {
+            return group.ToCharArray().Distinct().Count();
+        }
+
+
+        private static int CountSameLettersInGroup(this List<string> group)
+        {
+            //97-122 int counters[27]; //--> [0] ->a, [1] ->b .... [26]->z
+            int[] counters = new int[27];
+
+            foreach (byte[] answersByPerson in group.Select(s => Encoding.ASCII.GetBytes(s.ToLower())))
+            {
+                foreach (var asciiCharInCounterPosition in answersByPerson.Select(s => s - 97))
+                {
+                    counters[asciiCharInCounterPosition]++;
+                }
+            }
+
+            //obtenemos las preguntas que todos contestaron si, que serán las que tengan tantos sis como grupos,
+            //es decir que el contador de repeticiones sea igual al número de personas en el grupo.
+
+            return counters.Count(i => i == @group.Count);
+
+
+
+        }
 
     }
 
